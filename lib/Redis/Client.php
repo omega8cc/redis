36662223f17b70a0a528c8b747a527d87a0cfd55
Pierre Rineau <pierre.rineau@makina-corpus.com>
2015-04-08 13:35:11 +0200

    Moving static methods to Drupal compatibility layer

--- lib/Redis/Client.php
+++ lib/Redis/Client.php
@@ -51,6 +51,87 @@ class Redis_Client
     private static $manager;
 
     /**
+     * @var string
+     */
+    static protected $globalPrefix;
+
+    /**
+     * Get site default global prefix
+     *
+     * @return string
+     */
+    static public function getGlobalPrefix()
+    {
+        // Provide a fallback for multisite. This is on purpose not inside the
+        // getPrefixForBin() function in order to decouple the unified prefix
+        // variable logic and custom module related security logic, that is not
+        // necessary for all backends. We can't just use HTTP_HOST, as multiple
+        // hosts might be using the same database. Or, more commonly, a site
+        // might not be a multisite at all, but might be using Drush leading to
+        // a separate HTTP_HOST of 'default'. Likewise, we can't rely on
+        // conf_path(), as settings.php might be modifying what database to
+        // connect to. To mirror what core does with database caching we use
+        // the DB credentials to inform our cache key.
+      if (null === self::$globalPrefix) {
+            require_once DRUPAL_ROOT . '/includes/database/database.inc';
+            $dbInfo = Database::getConnectionInfo();
+            $active = $dbInfo['default'];
+            self::$globalPrefix = md5($active['host'] . $active['database'] . $active['prefix']['default']);
+        }
+
+        return self::$globalPrefix;
+    }
+
+    /**
+     * Get global default prefix
+     *
+     * @param string $namespace
+     *
+     * @return string
+     */
+    static public function getDefaultPrefix($namespace = null)
+    {
+        $ret = null;
+
+        if (isset($GLOBALS['drupal_test_info']) && !empty($test_info['test_run_id'])) {
+            $ret = $test_info['test_run_id'];
+        } else {
+            $prefixes = variable_get('cache_prefix', null);
+
+            if (is_string($prefixes)) {
+                // Variable can be a string which then considered as a default
+                // behavior.
+                $ret = $prefixes;
+            } else if (null !== $namespace && isset($prefixes[$namespace])) {
+                if (false !== $prefixes[$namespace]) {
+                    // If entry is set and not false an explicit prefix is set
+                    // for the bin.
+                    $ret = $prefixes[$namespace];
+                } else {
+                    // If we have an explicit false it means no prefix whatever
+                    // is the default configuration.
+                    $ret = '';
+                }
+            } else {
+                // Key is not set, we can safely rely on default behavior.
+                if (isset($prefixes['default']) && false !== $prefixes['default']) {
+                    $ret = $prefixes['default'];
+                } else {
+                    // When default is not set or an explicit false this means
+                    // no prefix.
+                    $ret = '';
+                }
+            }
+        }
+
+        if (empty($ret)) {
+            $ret = Redis_Client::getGlobalPrefix();
+        }
+
+        return $ret;
+    }
+
+    /**
      * Get client manager
      *
      * @return Redis_Client_Manager

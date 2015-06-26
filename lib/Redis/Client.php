c0a2b3ee524ac6ece58e9ad73cc65ab7331ef4ed
Pierre Rineau <pierre.rineau@makina-corpus.com>
2015-06-26 12:37:50 +0200

    Issue #2189275: (Cache Backport compat) Fatal error while requiring database.inc and  accessing \Database

--- lib/Redis/Client.php
+++ lib/Redis/Client.php
@@ -68,10 +68,17 @@ class Redis_Client
         // connect to. To mirror what core does with database caching we use
         // the DB credentials to inform our cache key.
       if (null === self::$globalPrefix) {
-            require_once DRUPAL_ROOT . '/includes/database/database.inc';
-            $dbInfo = Database::getConnectionInfo();
-            $active = $dbInfo['default'];
-            self::$globalPrefix = md5($active['host'] . $active['database'] . $active['prefix']['default']);
+            if (isset($GLOBALS['db_url'])) {
+                // Drupal 6 specifics when using the cache_backport module, we
+                // therefore cannot use \Database class to determine database
+                // settings.
+              self::$globalPrefix = md5($GLOBALS['db_url']);
+            } else {
+                require_once DRUPAL_ROOT . '/includes/database/database.inc';
+                $dbInfo = Database::getConnectionInfo();
+                $active = $dbInfo['default'];
+                self::$globalPrefix = md5($active['host'] . $active['database'] . $active['prefix']['default']);
+            }
         }
 
         return self::$globalPrefix;

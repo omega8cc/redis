568a16adf41cad21eb59723f2192fa4a9652c87f
Pierre Rineau <pierre.rineau@makina-corpus.com>
2015-08-23 12:22:44 +0200

    Redis_AbstractBackend::getKey() optimization

--- lib/Redis/AbstractBackend.php
+++ lib/Redis/AbstractBackend.php
@@ -80,14 +80,29 @@ abstract class Redis_AbstractBackend implements Redis_BackendInterface
      *
      * @return string
      */
-    public function getKey($parts = array())
+    public function getKey($parts = [])
     {
-        if (!is_array($parts)) {
-            $parts = array($parts);
+        $key = [];
+
+        if (null !== $this->prefix) {
+            $key[] = $this->prefix;
+        }
+        if (null !== $this->namespace) {
+            $key[] = $this->namespace;
         }
 
-        array_unshift($parts, $this->getPrefix(), $this->getNamespace());
+        if ($parts) {
+            if (is_array($parts)) {
+                foreach ($parts as $part) {
+                    if ($part) {
+                        $key[] = $part;
+                    }
+                }
+            } else {
+                $key[] = $parts;
+            }
+        }
 
-        return implode(self::KEY_SEPARATOR, array_filter($parts));
+        return implode(self::KEY_SEPARATOR, array_filter($key));
     }
 }

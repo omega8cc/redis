3ac59a34a37b7d2d7a4d91d7fcee5dfe0e42191b
Barracuda Team <admin@omega8.cc>
2019-08-20 22:52:40 +0200

    Revert "Redis 5 compatibility Issue #3074189"
    This reverts commit 0169fae839b2bdb924457f1e284ecbff09fe85d9.

--- lib/Redis/Lock/PhpRedis.php
+++ lib/Redis/Lock/PhpRedis.php
@@ -108,7 +108,7 @@ class Redis_Lock_PhpRedis extends Redis_Lock_DefaultBackend {
 
     if ($client->get($key) == $id) {
       $client->multi();
-      $client->del($key);
+      $client->delete($key);
       $client->exec();
     }
     else {
@@ -131,7 +131,7 @@ class Redis_Lock_PhpRedis extends Redis_Lock_DefaultBackend {
       $owner = $client->get($key);
 
       if (empty($owner) || $owner == $id) {
-        $client->del($key);
+        $client->delete($key);
       }
     }
   }

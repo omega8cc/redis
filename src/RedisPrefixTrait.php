98707e30dfc9e95348e67146ae946b08cbb14508
BOA Dev Team <boa@omega8.cc>
2019-09-05 19:09:10 +0200

    Simplify getDefaultPrefix()

--- src/RedisPrefixTrait.php
+++ src/RedisPrefixTrait.php
@@ -19,57 +19,19 @@ trait RedisPrefixTrait {
    * @return string
    */
   protected function getDefaultPrefix($suffix = NULL) {
-    $ret = NULL;
 
-    if ($test_prefix = drupal_valid_test_ua()) {
-      $ret = $test_prefix;
-    }
-    else {
-      $prefixes = Settings::get('cache_prefix', '');
+    $ret = ''; // Ignore prefix defined in global.inc or local.settings.php
 
-      if (is_string($prefixes)) {
-        // Variable can be a string which then considered as a default
-        // behavior.
-        $ret = $prefixes;
-      }
-      else if (NULL !== $suffix && isset($prefixes[$suffix])) {
-        if (FALSE !== $prefixes[$suffix]) {
-          // If entry is set and not false an explicit prefix is set
-          // for the bin.
-          $ret = $prefixes[$suffix];
-        }
-        else {
-          // If we have an explicit false it means no prefix whatever
-          // is the default configuration.
-          $ret = '';
-        }
-      }
-      else {
-        // Key is not set, we can safely rely on default behavior.
-        if (isset($prefixes['default']) && FALSE !== $prefixes['default']) {
-          $ret = $prefixes['default'];
-        }
-        else {
-          // When default is not set or an explicit false this means
-          // no prefix.
-          $ret = '';
-        }
-      }
-    }
-
-    if (empty($ret)) {
-      // If no prefix is given, use the same logic as core for APCu caching.
-      $ret = Settings::getApcuPrefix('redis', DRUPAL_ROOT);
-    }
-
-    if (isset($_SERVER['MAIN_SITE_NAME'])) {
-      $ret = md5(preg_replace('`^www\.`', '', $_SERVER['MAIN_SITE_NAME'])) . '_m_';
-    }
-    elseif (isset($_SERVER['SERVER_NAME'])) {
+    if (isset($_SERVER['SERVER_NAME'])) {
       $ret = md5(preg_replace('`^www\.`', '', $_SERVER['SERVER_NAME'])) . '_n_';
     }
     elseif (isset($_SERVER['HTTP_HOST'])) {
       $ret = md5(preg_replace('`^www\.`', '', $_SERVER['HTTP_HOST'])) . '_h_';
+    }
+
+    if (empty($ret)) {
+      // If still no prefix is given, use the same logic as core for APCu caching.
+      $ret = Settings::getApcuPrefix('redis', DRUPAL_ROOT);
     }
 
     return $ret;
@@ -81,7 +43,12 @@ trait RedisPrefixTrait {
    * @param string $prefix
    */
   public function setPrefix($prefix) {
-    $this->prefix = $prefix;
+    if (!isset($this->prefix)) {
+      $this->prefix = $this->getDefaultPrefix();
+    }
+    else {
+      $this->prefix = $prefix;
+    }
   }
 
   /**
